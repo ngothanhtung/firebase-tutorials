1ca60d83a4e40a40569357264c34e9cb07728cfa
Ngo Thanh Tung <tung.ngo@outlook.com>
2018-08-12 16:07:46 +0700

    Update CLOUD F

--- firebase-cloudfirestore/CloudFireStoreHelper.js
+++ firebase-cloudfirestore/CloudFireStoreHelper.js
@@ -7,19 +7,19 @@ class CloudFireStoreHelper {
 	// Add Document
 	add() {
 		var data = {
-			'title': 'Vung Tau City',		
+			'title': 'Vung Tau City',
 			'subTitle': 'Phia Nam',
 			'text': 'Thanh pho Bien xinh dep',
 			'imageUrl': "https://images.unsplash.com/photo-1446770145316-10a05382c470?auto=format&fit=crop&w=1950&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D"
 		};
-	
+
 		var docRef = this.db.collection('images').add(data);
 	}
 
 	// Update Document
 	update() {
 		var docRef = this.db.collection('images').doc("POKuvbni0KBlXGj0wwdr")
-	
+
 		var result = docRef.set({
 			'title': 'Danang City',
 			'subTitle': 'Beautiful city',
@@ -27,6 +27,27 @@ class CloudFireStoreHelper {
 			'imageUrl': "https://images.unsplash.com/photo-1425342605259-25d80e320565?auto=format&fit=crop&w=1950&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D"
 		});
 	}
+
+	// Find And update Document
+	findAndUpdate(customerId) {
+		var docRef = this.db.collection('Notifications');
+		var action = this.db.collection('Notifications')
+			.where('customerId', '==', customerId).get()
+			.then((snapshot) => {
+				if (!snapshot.exists) {
+					console.log('No such document!');
+					docRef.add({ customerId: customerId, notificationCount: 1 }).then(result => console.log(result));
+				} else {
+					console.log('Document data:', snapshot.data());
+					// doc.set({ customerId: customerId, notificationCount: 1 }).then(result => console.log(result));
+				}
+			})
+			.catch((error) => {
+				console.log('Error', error);
+				res.json(error);
+			});
+	}
+
 
 	// Update Document Fields
 	updateField() {
